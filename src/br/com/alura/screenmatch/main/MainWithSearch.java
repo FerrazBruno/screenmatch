5264f998d6f97b399d2439978fe873e0ef83c5be
Bruno Ferraz <brunohenriqueferraz94@gmail.com>
2024-08-19 22:18:15 -0300

    using a http request library

---
+++ src/br/com/alura/screenmatch/main/MainWithSearch.java
@@ -0,0 +1,25 @@
+package br.com.alura.screenmatch.main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class MainWithSearch {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type the movie to search: ");
+        var search = input.next();
+        String apiKey = "3cc7e0de";
+        String address = "http://www.omdbapi.com/?t=" + search + "&apikey=" + apiKey;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(address))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
